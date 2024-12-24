3db6acd06ea9191894576704afa5cabd47b71579
lovindy <seangdy085@gmail.com>
2024-12-24 03:58:26 -0800

    add grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import { BackgroundLines } from "./BackgroundLine";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
     className,
@@ -24,8 +26,6 @@ export const BentoGridItem = ({
     className,
     title,
     description,
-    header,
-    icon,
     id,
     img,
     imgClassName,
@@ -35,8 +35,6 @@ export const BentoGridItem = ({
     className?: string;
     title?: string | React.ReactNode;
     description?: string | React.ReactNode;
-    header?: React.ReactNode;
-    icon?: React.ReactNode;
     id?: number;
     img?: string;
     imgClassName?: string;
@@ -46,13 +44,10 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
-                "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+                "text-blue-100 row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
                 className
             )}
             style={{
-                //   add these two
-                //   you can generate the color from here https://cssgradient.io/
                 background: "rgb(4,7,29)",
                 backgroundColor:
                     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
@@ -60,18 +55,46 @@ export const BentoGridItem = ({
         >
             <div className={`${id === 6} && 'flex justify-center h-full`}>
                 <div className="w-full h-full absolute">
-
+                    {img && (
+                        <img
+                            src={img}
+                            alt={img}
+                            className={cn(imgClassName, 'object-cover, object-center')}
+                        />
+                    )}
                 </div>
-
-            </div>
-
-            <div className="group-hover/bento:translate-x-2 transition duration-200">
-
-                <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-                    {title}
+                <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+                    {
+                        spareImg && (
+                            <img
+                                src={spareImg}
+                                alt={spareImg}
+                                className="w-full h-full object-cover object-center"
+                            />
+                        )
+                    }
                 </div>
-                <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-                    {description}
+                {id === 6 && (
+                    // add background animation , remove the p tag
+                    <BackgroundGradientAnimation>
+                        <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+                    </BackgroundGradientAnimation>
+                )}
+
+                <div
+                    className={cn(
+                        titleClassName,
+                        "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+                    )}
+                >
+                    <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+                        {description}
+                    </div>
+                    <div
+                        className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+                    >
+                        {title}
+                    </div>
                 </div>
             </div>
         </div>
